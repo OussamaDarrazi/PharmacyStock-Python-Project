7574f08da6a6eef8d1fbcefa7aed8c57a699cea3
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2023-12-23 22:47:49 +0100

    init repo - model structures

---
+++ ClassesUtils.py
@@ -0,0 +1,20 @@
+#### for structure of data
+
+class medicament:
+    def __init__(self):
+        self.id=0
+        self.name=""
+        self.supplier=""
+        self.quantite=0
+        self.description=""
+        self.category=""
+        self.ordonnance=False
+
+class category:
+    def __init__(self):
+        self.name=""
+
+class supplier:
+    def __init__(self):
+        self.name=""
+
